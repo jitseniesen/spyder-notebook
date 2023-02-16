ef490f8c3ff1205095f1bd90684ae14163043e7f
Jitse Niesen <jitseniesen@yahoo.com>
2023-02-16 09:11:10 +0000

    WIP Add constructor to SpyderNotebookApp
    Constructor has a silly alert() to show that it is being called

--- spyder_notebook/server/packages/application/src/app.ts
+++ spyder_notebook/server/packages/application/src/app.ts
@@ -1,7 +1,17 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { NotebookApp } from '@jupyter-notebook/application';
+import { NotebookApp, NotebookShell } from '@jupyter-notebook/application';
 
 export class SpyderNotebookApp extends NotebookApp {
+  /**
+   * Construct a new SpyderNotebookApp object.
+   *
+   * @param options The instantiation options for an application.
+   */
+  constructor(options: NotebookApp.IOptions = { shell: new NotebookShell() }) {
+    super({ ...options, shell: options.shell ?? new NotebookShell() });
+
+    alert('SpyderNotebookApp constructed!');
+  }
 }
